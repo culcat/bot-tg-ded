ade00bca6ab553b36adf057c844de59ca49c9dd5
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2023-08-27 12:43:42 +0300

    разобрался с клавиатурой и выводом

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import secrets
 import telebot
 import psycopg2
+from telebot import types
 
 conn = psycopg2.connect(dbname="tg", host="localhost", user="postgres", password="postgrespw", port="32770")
 cursor = conn.cursor()
 conn.autocommit = True
 
-cursor.execute("SELECT botkey FROM botsettings")
+cursor.execute("SELECT botkey FROM botsettings where botsettingid = 1")
 token_row = cursor.fetchone()
 token = str(token_row[0])
 bot = telebot.TeleBot(token)
 
-
+cursor.execute("SELECT botkey FROM botsettings where botsettingid = 2")
+adminToken_row = cursor.fetchone()
+adminToken = str(adminToken_row[0])
+cursor.execute("SELECT adminuserid FROM botsettings where botsettingid = 2")
+adminUserId_row = cursor.fetchone()
+adminUserID = str(adminUserId_row[0])
+def create_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_invite = types.KeyboardButton('/invite')
+    button_balance = types.KeyboardButton('/balance')
+    button_withdraw = types.KeyboardButton('/withdraw')
+    keyboard.add(button_invite, button_balance, button_withdraw)
+    return keyboard
 @bot.message_handler(commands=['invite'])
 def invite_message(message):
     user = message.from_user
@@ -72,7 +85,7 @@ def start_message(message):
     else:
         response = "Пригласитель не найден."
 
-    bot.reply_to(message, response)
+    bot.reply_to(message, response, reply_markup=create_keyboard())
 
 
 @bot.message_handler(commands=['balance'])
@@ -95,7 +108,7 @@ def start_message(message):
 
 
 @bot.message_handler(commands=['withdraw'])
-def balance_message(message):
+def withdraw_request(message):
     user = message.from_user
     user_id = user.id
     cursor.execute("SELECT balance FROM users WHERE tgid = %s", (str(user_id),))
@@ -104,13 +117,48 @@ def balance_message(message):
     if balance_row:
         balance = balance_row[0]
         if balance >= 500:
-            response = f"Подать заявку на вывод\n"
+            bot.send_message(user_id, "Введите сумму для вывода:")
+            bot.register_next_step_handler(message, process_withdrawal)
         else:
-            response = "У вас недостаточно средств для вывода баланса."
+            bot.reply_to(message, "У вас недостаточно средств для вывода баланса.")
     else:
-        response = "Пользователь не найден в базе данных."
+        bot.reply_to(message, "Пользователь не найден в базе данных.")
 
-    bot.reply_to(message, response)
+@bot.message_handler(func=lambda message: True)
+def process_withdrawal(message):
+    try:
+        withdrawal_amount = float(message.text)
+        user_id = message.from_user.id
 
+        cursor.execute("SELECT balance FROM users WHERE tgid = %s", (str(user_id),))
+        balance_row = cursor.fetchone()
 
+        if balance_row:
+            balance = balance_row[0]
+            if withdrawal_amount <= balance and withdrawal_amount >= 500:  # Добавлено ограничение до 500
+                # Отправка данных админскому боту
+                admin_user_id = "326646054"
+                admin_message = f"Пользователь {user_id} хочет вывести {withdrawal_amount}.\nТекущий баланс: {balance}"
+                send_admin_notification(admin_message)
+
+                # Обновление баланса пользователя в базе данных
+                # Обновление баланса пользователя в базе данных
+                new_balance = float(balance) - withdrawal_amount
+                cursor.execute("UPDATE users SET balance = %s WHERE tgid = %s", (new_balance, str(user_id)))
+                conn.commit()
+
+                bot.send_message(user_id, "Заявка на вывод отправлена администратору. Ваш баланс обновлен.")
+            else:
+                bot.send_message(user_id, "Недостаточно средств для вывода указанной суммы или сумма меньше минимальной (500).")
+        else:
+            bot.send_message(user_id, "Пользователь не найден в базе данных.")
+    except ValueError:
+        user_id = message.from_user.id
+        bot.send_message(user_id, "Введите корректную сумму.")
+
+# Функция для отправки уведомления админу
+def send_admin_notification(message):
+    admin_bot = telebot.TeleBot(adminToken)
+    admin_user_id = adminUserID
+    admin_bot.send_message(admin_user_id, message)
 bot.infinity_polling()
